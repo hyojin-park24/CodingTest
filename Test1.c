15e29d549496ff2210ddacdd6cddf7aca3b909c0
hyojin-park24 <gygy332@naver.com>
2021-05-12 23:54:49 +0900

    초기 커밋.

---
+++ Test1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+   
+    printf("두 정수를 입력하세요 : ");
+    scanf("%d %d", &a, &b);
+    while (1)
+    {
+        char ch;
+        scanf("%c", &ch);
+        if (ch == 'F') break;
+        
+        if (a > b)
+        {
+            printf("Yes \n");
+        }
+        else if (a == 0 && b == 0)
+        {
+            printf("Yes \n");
+            break;
+        }
+        
+        else
+        {
+            printf("No \n");
+        }
+        printf("그만하고 싶다면 F를 누르시오. \n");
+        printf("다시 입력하세요 : \n");
+        scanf("%d %d", &a, &b);
+
+    }
+
+    return 0;
+}
